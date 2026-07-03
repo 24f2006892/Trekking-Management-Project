52787afbd076c95914952f543335212c2975e9fa
Anurag Prajapati <24f2006892@ds.study.iitm.ac.in>
2026-07-03 16:19:11 +0000

    Bookings backend and templates

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask , render_template , redirect,request,session
 from werkzeug.security import generate_password_hash , check_password_hash
 import sqlite3
+import os
+
 
 app = Flask(__name__)
 def get_db_connnection():
@@ -57,11 +59,54 @@ def login():
         if user and check_password_hash(user["password"],password):
             session["user"]=username
             return("Login Successful")
+        else:
+            return("Invalid Details")
+
         
 
-@app.route("/register")
-def login():
-    return "Register"
+@app.route("/register" , methods=["GET", "POST"])
+def register():
+    if request.method=="POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        hashed_password = generate_password_hash(password)
+        conn = get_db_connnection()
+        conn.execute(""" 
+                    INSERT INTO users (username, email, password) VALUES (?, ?, ?)""",(username, email, hashed_password))
+        conn.commit()
+        conn.close()
+    return "User Registered Successfully"
+
+# View treks backend 
+@app.route("view_treks")
+def view_treks():
+    return "This is a trek"
+#Book trek - user side
+@app.route("/book/<int:trek_id>", methods=["POST"])
+def book_trek(trek_id):
+    username = session["user"]
+    conn = get_db_connnection()
+    #check trek
+    trek = conn.execute(""" 
+                        SELECT * FROM treks WHERE  id=?   """,(trek_id)).fetchone
+    if not trek or trek["slots"]<=0:
+        conn.close()
+        return "No treks Available"
+    # insert bookings
+    conn.execute(" INSERT INTO bookings (username, trek_id) VALUES (?, ?)",
+                 (username, trek_id))
+    # Update slots
+    conn.execute(
+        " UPDATE treks SET slots = slots - 1 WHERE id = ? AND slots > 0",
+        (trek_id,)
+    )
+    conn.commit()
+    conn.close()
+
+
+
 
 if __name__ == '__main__' :
     app.run(debug=True)
